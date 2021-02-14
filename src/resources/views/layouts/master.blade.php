cf537e55113d18468ce2893307dfbb0bce909d80
juuhgouvea <juliasantos752@yahoo.com.br>
2021-02-14 02:08:10 -0300

    estilizacao das fontes e configuracao de navegacao

--- src/resources/views/layouts/master.blade.php
+++ src/resources/views/layouts/master.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
     <link rel="shortcut icon" href="{{ url('images/curso_ico.png') }}" type="image/png">
     <link rel="stylesheet" href="{{ url('css/app.css') }}">
     <link rel="stylesheet" href="{{ url('css/main.css') }}">
@@ -16,7 +18,9 @@
             <div class="d-flex align-items-center justify-content-center">
                 @yield('pagina-ativa')
             </div>
-            <h1 class="titulo">SISAR - Sistema de Avaliação Remota</h1>
+            <a class="titulo" href="{{ url('/') }}">
+                SISAR - Sistema de Avaliação Remota
+            </a>
             <div class="d-flex align-items-center justify-content-center">
                 @yield('menu-nav-direito')
             </div>
@@ -25,7 +29,7 @@
 
     @yield('conteudo')
 
-    @yield('scripts')
     <script src="{{ url('js/app.js') }}"></script>
+    @yield('scripts')
 </body>
 </html>
