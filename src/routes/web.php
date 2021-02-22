b00bd21b94b35dc74ff297aac140984d0481437d
juuhgouvea <juliasantos752@yahoo.com.br>
2021-02-22 13:40:16 -0300

    atribuicao do middleware para as rotas

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,28 +15,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->middleware('access_level:0');
+
 
 Route::resource('curso', 'App\Http\Controllers\CursosController')
     ->only(['index', 'store', 'update'])
-    ->parameters(['curso' => 'id']);
+    ->parameters(['curso' => 'id'])
+    ->middleware('access_level:1');
 
 Route::resource('professor', 'App\Http\Controllers\ProfessoresController')
     ->only(['index', 'store', 'update'])
-    ->parameters(['professor' => 'id']);
+    ->parameters(['professor' => 'id'])
+    ->middleware('access_level:2');
 
 Route::resource('disciplina', 'App\Http\Controllers\DisciplinasController')
     ->only(['index', 'store', 'update'])
-    ->parameters(['disciplina' => 'id']);
+    ->parameters(['disciplina' => 'id'])
+    ->middleware('access_level:1');
 
 Route::resource('aluno', 'App\Http\Controllers\AlunosController')
     ->only(['index', 'store', 'update'])
-    ->parameters(['aluno' => 'id']);
+    ->parameters(['aluno' => 'id'])
+    ->middleware('access_level:2');
 
 Route::resource('matricula', 'App\Http\Controllers\MatriculasController')
     ->only(['index', 'update'])
-    ->parameters(['matricula' => 'aluno_id']);
+    ->parameters(['matricula' => 'aluno_id'])
+    ->middleware('access_level:2');
 
 Route::get('/negado', function() {
     return view('restrito');
-});
+})->name('negado');
